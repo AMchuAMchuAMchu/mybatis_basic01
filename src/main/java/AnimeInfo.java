c9455c0e2b34a23bfe20635072dea88efe1d9282
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-03 11:26:26 +0800

    AM 01st commit

---
+++ src/main/java/AnimeInfo.java
@@ -0,0 +1,24 @@
+import lombok.Data;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> mybatis_basic01
+ * BelongsPackage ==> PACKAGE_NAME
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-03 11:23:29
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@Data
+public class AnimeInfo {
+
+    private String name;
+
+    private Integer releaseTime;
+
+    private String character01;
+
+    private String character02;
+
+
+
+}
